997543033a345d6540a6eb740339c6d12805c2a7
havchr <havchr@gmail.com>
2021-05-25 21:49:32 +0200

    Add files via upload

---
+++ Editor/RuleDBEditor.cs
@@ -0,0 +1,168 @@
+﻿#if UNITY_EDITOR
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using FactMatcher;
+using UnityEditor;
+using UnityEngine;
+
+
+[CustomEditor(typeof(RulesDB))]
+public class RuleDBEditor : Editor
+{
+    private RulesDB _rulesDB;
+
+    private void OnEnable()
+    {
+        _rulesDB = (RulesDB)target;
+    }
+
+    public override void OnInspectorGUI()
+    {
+        base.OnInspectorGUI();
+        if(GUILayout.Button("Generate From Rulescripts")) 
+        {
+            GenerateFromText();
+        }    
+    }
+    
+    [MenuItem("Assets/Create/FactMatcher/RuleScript")]
+    private static void CreateRuleScript(MenuCommand command)
+    {
+
+        var fileName = "ruleScript_";
+        var dirPath = AssetDatabase.GetAssetPath(Selection.activeObject);
+        var path = dirPath +"/" + fileName ;
+        var assets = AssetDatabase.FindAssets(fileName,new []{dirPath});
+        int fileNumber = 1;
+        foreach (var assetGUID in assets)
+        {
+            var assetPath = AssetDatabase.GUIDToAssetPath(assetGUID);
+            Debug.Log(assetPath);
+
+            var splitted = assetPath.Split(new []{fileName},StringSplitOptions.RemoveEmptyEntries);
+
+            for (int i = 0; i < splitted.Length; i++)
+            {
+                Debug.Log($"split {i} is {splitted[i]}");
+            }
+            var number = splitted[1];
+            if (splitted[1].Contains("."))
+            {
+                number = splitted[1].Split('.')[0];
+                Debug.Log($"Number is {number}");
+            }
+            if(int.TryParse(number, out int potentialFileNumber))
+            {
+                if (potentialFileNumber > fileNumber)
+                {
+                    fileNumber = potentialFileNumber;
+                }
+            }
+
+        }
+        fileNumber++;
+        var finalPath = path + $"{fileNumber}.txt";
+        File.WriteAllText(Application.dataPath + finalPath.Split(new []{"Assets"},StringSplitOptions.RemoveEmptyEntries)[0] , GetDefaultRuleScriptContent());
+        AssetDatabase.Refresh();
+		
+    }
+
+    static string GetDefaultRuleScriptContent()
+    {
+        var rule1 = ".RuleName\n" +
+                    "player.age > 10\n" +
+                    "player.name = Johnny Lemon\n" +
+                    ":Response:\n" +
+                    "rule matches if age is bigger than 10 and name is Johnny Lemon\n" +
+                    ":End:\n\n\n";
+
+        var comment = "-- A comment starts with -- , but, remark,it is not handled within the response block..\n" +
+                      "-- Variables start with a namespace. If none is given, it will be automatically be given global as the namespace\n" +
+                      "-- The variables defined here will end up in generated c# code in FactMatcherCodeGenerator.cs\n" +
+                      "-- Everything is case-sensitive..\n" +
+                      "-- Deriving another rule allows you to copy all the checks of that rule. See rule below for an example\n\n\n"; 
+        
+        var rule2 = ".RuleName.Derived\n" +
+                    "player.height > 180\n" +
+                    "--You can also use Range which expands into to checks when the rulescript is parsed ( for exclusive [ for inclusive ..\n" +
+                    "player.health Range(5,25]\n" +
+                    ":Response:\n" +
+                    "rule matches if base rule .RuleName matches and height is bigger than 180\n" +
+                    ":End:\n\n\n";
+
+        return rule1 + comment + rule2;
+    }
+
+	
+    private void GenerateFromText()
+    {
+        if (_rulesDB.generateFrom != null)
+        {
+            _rulesDB.rules.Clear();
+            int factID = 0;
+            int ruleID = 0;
+            Dictionary<string,int> addedFactIDS = new Dictionary<string, int>();
+            foreach (var ruleScript in _rulesDB.generateFrom)
+            {
+                var parser = new RuleScriptParser();
+                parser.GenerateFromText(ruleScript.text,_rulesDB.rules,ref factID,ref addedFactIDS, ref ruleID); 
+            }
+            GenerateFactIDS();
+        }
+    }
+
+    
+    private void GenerateFactIDS()
+    {
+        EditorUtility.SetDirty(_rulesDB);
+        AssetDatabase.SaveAssets();
+        AssetDatabase.Refresh();
+
+        var generatedName = StripNameIntoCamelCase(_rulesDB.name);
+        var fileName = $"FactMatcher/Generated/{generatedName}.cs";
+        FactMatcherCodeGenerator.GenerateFactIDS(fileName, GetNameSpaceName(), _rulesDB);
+    }
+
+    public string GetNameSpaceName()
+    {
+        return $"FactMatcher_{StripNameIntoCamelCase(_rulesDB.name)}_Gen";
+    }
+
+        
+    //turns a string like "John Apple banana.power" into "JohnAppleBananaPower"
+    public static string StripNameIntoCamelCase(string name)
+    {
+
+        try
+        {
+
+            var stripEm = new[] {' ', '.'};
+            StringBuilder genName = new StringBuilder();
+            foreach (var strippy in stripEm)
+            {
+                genName.Clear();
+                var splitted = name.Split(strippy);
+                foreach (var s in splitted)
+                {
+                    genName.Append(s.Substring(0, 1).ToUpper());
+                    genName.Append(s.Substring(1, s.Length - 1));
+                }
+
+                name = genName.ToString();
+            }
+
+            return name;
+
+        }
+        catch (Exception e)
+        {
+            Debug.LogError($"Failed Stripping Name Into Camel case for {name} with exception {e.ToString()}");
+            return name;
+        }
+    }
+    
+}
+#endif
---
+++ Runtime/RulesDB.cs
@@ -0,0 +1,193 @@
+﻿using System;
+using System.Collections.Generic;
+using FactMatcher;
+using UnityEngine;
+
+public enum FactValueType
+{
+    String,Value	
+}
+
+[Serializable]
+public class RuleDBFactWrite
+{
+    
+    public int factID;
+    public string factName;
+    public string writeString;
+    public float writeValue;
+    public WriteMode writeMode = WriteMode.SetString;
+
+    public enum WriteMode
+    {
+        SetString,SetValue,IncrementValue,SubtractValue
+    }
+    
+}
+
+[Serializable]
+public class RuleDBAtomEntry
+{
+    public int factID;
+    public string factName;
+    public string matchString;
+    public float matchValue;
+    public Comparision compareMethod;
+    public FactValueType compareType;
+
+    public enum Comparision
+    {
+        Equal,NotEqual,LessThan,MoreThan,LessThanEqual,MoreThanEqual,Range
+    }
+
+    public FactMatcher.RuleCompare CreateCompare(RulesDB rules)
+    {
+        //used in the fact system 
+        var val = compareType == FactValueType.String ?  rules.StringId(matchString) : matchValue;
+        switch (compareMethod)
+        {
+            case Comparision.Equal:
+                return FactMatcher.RuleCompare.Equals(val);
+            case Comparision.NotEqual:
+                return FactMatcher.RuleCompare.NotEquals(val);
+            case Comparision.LessThan:
+                return FactMatcher.RuleCompare.LessThan(val);
+            case Comparision.LessThanEqual:
+                return FactMatcher.RuleCompare.LessThanEquals(val);
+            case Comparision.MoreThan:
+                return FactMatcher.RuleCompare.MoreThan(val);
+            case Comparision.MoreThanEqual:
+                return FactMatcher.RuleCompare.MoreThanEquals(val);
+        }
+        return FactMatcher.RuleCompare.Equals(val);
+    }
+}
+
+[Serializable]
+public class RuleDBEntry
+{
+    public int RuleID;
+    public string ruleName;
+    public string payload;
+    public List<RuleDBFactWrite> factWrites;
+    public List<RuleDBAtomEntry> atoms;
+}
+
+[CreateAssetMenu(fileName = "RulesDB", menuName = "FactMatcher/RulesDB", order = 1)]
+public class RulesDB : ScriptableObject
+{
+    
+    private Dictionary<string, int> RuleStringMap;
+    private Dictionary<int, RuleDBEntry> RuleMap;
+    public List<TextAsset> generateFrom;
+    public List<RuleDBEntry> rules;
+    
+    public void InitRuleDB()
+    {
+        RuleStringMap = RuleStringIDs(this);
+        RuleMap = CreateEntryFromIDDic(this);
+    }
+    public int StringId(string str)
+    {
+        if (RuleStringMap == null)
+        {
+            InitRuleDB();
+        }
+        int id = -1;
+        if (!RuleStringMap.TryGetValue(str, out id))
+        {
+            id = -1;
+        }
+        return id;
+    }
+    
+    public RuleDBEntry RuleFromID(int id)
+    {
+        if (RuleMap == null)
+        {
+            InitRuleDB();
+        }
+
+        RuleDBEntry rule;
+        if (!RuleMap.TryGetValue(id, out rule))
+        {
+            rule = null;
+        }
+        return rule;
+    }
+
+    private static Dictionary<int, RuleDBEntry> CreateEntryFromIDDic(RulesDB current)
+    {
+        Dictionary<int,RuleDBEntry>  dic = new Dictionary<int,RuleDBEntry>();
+        foreach (var rule in current.rules)
+        {
+            var id = rule.RuleID;
+            dic[id] = rule;
+        }
+        return dic;
+    }
+
+    private static Dictionary<string, int> RuleStringIDs(RulesDB current)
+    {
+        Dictionary<string, int>  dic = new Dictionary<string, int>();
+        
+        int id = 0;
+        foreach (var rule in current.rules)
+        {
+            foreach (var factWrite in rule.factWrites)
+            {
+                if (factWrite.writeMode == RuleDBFactWrite.WriteMode.SetString 
+                && factWrite.writeString!=null && factWrite.writeString.Length>=1)
+                {
+                    
+                    if (!dic.ContainsKey(factWrite.writeString))
+                    {
+                        dic[factWrite.writeString] = id;
+                        id++;
+                    }
+                }
+            }
+
+            foreach (var atom in rule.atoms)
+            {
+                if (atom.compareType == FactValueType.String && atom.matchString!=null && atom.matchString.Length>=1)
+                {
+                    if (!dic.ContainsKey(atom.matchString))
+                    {
+                        dic[atom.matchString] = id;
+                        id++;
+                    }
+                }
+            }
+        }
+        
+        return dic;
+    }
+    
+	public int CountNumberOfFacts()
+	{
+		int topFactID = 0;
+		foreach (var rule in rules)
+		{
+
+			foreach (var atom in rule.atoms)
+			{
+				if (atom.factID > topFactID)
+				{
+					topFactID = atom.factID;
+				}
+			}
+			
+			foreach (var factWrite in rule.factWrites)
+			{
+				if (factWrite.factID > topFactID)
+				{
+					topFactID = factWrite.factID;
+				}
+			}
+		}
+		return topFactID + 1;
+	}
+    
+	
+}
